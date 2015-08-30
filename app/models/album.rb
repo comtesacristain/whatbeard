5145b48da636b6d0c76b5a3a9c96beb1e53212be
Michael Sexton <mick.sexton@gmail.com>
2015-08-30 10:10:00 +1000

    Added recommendations model

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,6 @@
 class Album < ActiveRecord::Base
   belongs_to :artist
   has_many :tracks
+  
+  has_many :recommendations, as: :recommended
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,6 @@ class Artist < ActiveRecord::Base
   has_many :albums
   has_many :album_tracks, :through => :albums, :source => :tracks
   has_many :tracks
+  
+  has_many :recommendations, as: :recommended
 end

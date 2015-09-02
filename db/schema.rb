0aeb4b7d99c046002d2d6c1b48ebbecacdd7ffc8
Michael Sexton <mick.sexton@gmail.com>
2015-09-02 21:43:42 +1000

    Added album covers

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824091613) do
+ActiveRecord::Schema.define(version: 20150831114600) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
     t.integer  "artist_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "lastfm_url"
+    t.string   "cover_file_name"
+    t.string   "cover_content_type"
+    t.integer  "cover_file_size"
+    t.datetime "cover_updated_at"
   end
 
   create_table "artists", force: :cascade do |t|

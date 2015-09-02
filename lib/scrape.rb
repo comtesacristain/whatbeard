7b13f47ff8694fd046fc38c84d83133dc11d9c96
Michael Sexton <mick.sexton@gmail.com>
2015-09-02 21:53:20 +1000

    Scrape for album covers

--- lib/scrape.rb
+++ lib/scrape.rb
@@ -2,15 +2,22 @@ scraper = LastfmScraper.new
 scraper.login
 artists=scraper.artists
 artists.each do |a|
+  puts a
   artist = Artist.find_or_create_by(title:a[:artist])
+  artist.lastfm_url=a[:url]
   recommendation=artist.recommendations.find_or_create_by(source:"last_fm")
+  artist.save
 end
 
 albums=scraper.albums
 
 albums.each do |a|
+  puts a
   artist = Artist.find_or_create_by(title:a[:artist])
   artist.recommendations.find_or_create_by(source:"last_fm")
   album = artist.albums.find_or_create_by(title:a[:album])
+  album.lastfm_url=a[:url]
+  album.cover_from_url = a[:image_url]
   album.recommendations.find_or_create_by(source:"last_fm")
+  album.save
 end
